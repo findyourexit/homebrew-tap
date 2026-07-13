f6733f6cb8437d27b1ff610a602b455aac1a0ac0
Tom Larcher <tom.larcher@gmail.com>
2026-07-13 18:57:09 +1000

    chore(release): `ztl` version `0.3.0`

--- Formula/ztl.rb
+++ Formula/ztl.rb
@@ -2,15 +2,15 @@ class Ztl < Formula
   desc "A terminal UI for comparing multiple time zones side by side"
   homepage "https://github.com/findyourexit/zonetimeline-tui"
   license "MIT"
-  version "0.2.0"
+  version "0.3.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/findyourexit/zonetimeline-tui/releases/download/v0.2.0/ztl-0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "3029174a789a6f1fc68720cc1ecd9f50152cbeb8d30986617186187df04a5911"
+      url "https://github.com/findyourexit/zonetimeline-tui/releases/download/v0.3.0/ztl-0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "03e2645581a32a843de2910af7b2698e12c5a84d5c03c872a45f4612bd1fee78"
     else
-      url "https://github.com/findyourexit/zonetimeline-tui/releases/download/v0.2.0/ztl-0.2.0-x86_64-apple-darwin.tar.gz"
-      sha256 "bb4e157310d53a451eeff0283214f1a693d84b85386cf3c78d76e813e77cf663"
+      url "https://github.com/findyourexit/zonetimeline-tui/releases/download/v0.3.0/ztl-0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "b997364cd506fbea9cbbb048bdabe824bedfc34b9ab3a0e565e18f7ae372c581"
     end
   end
 
