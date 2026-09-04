bd7c7ec8589ee8790fa74608fb78011efe26f3ef
findyourexit release automation <release-automation@findyourexit.invalid>
2026-09-04 12:30:32 +0000

    excise 1.2.2

--- Formula/excise.rb
+++ Formula/excise.rb
@@ -8,21 +8,21 @@ class Excise < Formula
   license "MIT"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/findyourexit/excise/releases/download/v1.2.1/excise-aarch64-apple-darwin-v1.2.1.tar.gz"
-      sha256 "3e1382e5fdc4329750728b1a0d4d8463625e0fa4a5a2d60264790a021b0c9f4e"
+      url "https://github.com/findyourexit/excise/releases/download/v1.2.2/excise-aarch64-apple-darwin-v1.2.2.tar.gz"
+      sha256 "4174ac76f790f4a836de6466ec94668c5bfa29820e384af9eca7335581ae5b33"
     else
-      url "https://github.com/findyourexit/excise/releases/download/v1.2.1/excise-x86_64-apple-darwin-v1.2.1.tar.gz"
-      sha256 "ec1cda1c2d99cfaba6fc750141ba57b500cc78c73fdceb1f5046d73f864aeba4"
+      url "https://github.com/findyourexit/excise/releases/download/v1.2.2/excise-x86_64-apple-darwin-v1.2.2.tar.gz"
+      sha256 "22384591022f998de51e85dc9bffacaec6aefc16307f0d83e69e01d4b8d52219"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/findyourexit/excise/releases/download/v1.2.1/excise-aarch64-unknown-linux-gnu-v1.2.1.tar.gz"
-      sha256 "e259f53885f33a8a5b64b459990f4c755845e05bc98be67f820541755804560c"
+      url "https://github.com/findyourexit/excise/releases/download/v1.2.2/excise-aarch64-unknown-linux-gnu-v1.2.2.tar.gz"
+      sha256 "97f1c065d59fe6641365fb22550cff9737dc4c3902f3873a178eba8c652947fe"
     else
-      url "https://github.com/findyourexit/excise/releases/download/v1.2.1/excise-x86_64-unknown-linux-gnu-v1.2.1.tar.gz"
-      sha256 "df039dd469deddbfe0c378ad58c68f2645696f9554c3686f8f5b7031d6b0dec4"
+      url "https://github.com/findyourexit/excise/releases/download/v1.2.2/excise-x86_64-unknown-linux-gnu-v1.2.2.tar.gz"
+      sha256 "98b2de6a7364473336fbe3dc51e42ef47e4d0cf641ec663d24890c44350944aa"
     end
   end
 
